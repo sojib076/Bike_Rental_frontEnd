2c58baa4b007377dac553c7176d7048ef35b1696
sojib076 <sojibds332@gmail.com>
2024-08-25 10:12:16 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+
+import GoToTopButton from "./components/CommonComponents/GoToTopButton";
+import MainLayout from "./Layout/MainLayout";
+// import app css
+import './App.css';
+const App = () => {
+
+  return (
+  <div>
+     <MainLayout/>
+     <GoToTopButton/>
+  </div>
+  );
+};
+
+export default App;
